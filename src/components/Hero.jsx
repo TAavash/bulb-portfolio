a8cd8faff6b411c2fd890da000eed650bb778408
AAVASH <aavashlopchan@gmail.com>
2025-06-04 19:58:07 +0545

    navbar created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
 import Bulb from "./Bulb";
+import Navbar from "./Navbar";
 import Technologies from "./Technologies";
 
 const Hero = ({ mode, setMode }) => {
-const gradient =
+  const gradient =
     mode === "light"
       ? "linear-gradient(180deg, #ccffcc 0%, #f2fbe0 100%)"
       : mode === "red"
@@ -18,9 +19,13 @@ const gradient =
 
   return (
     <div
-  className={`relative w-screen h-screen flex items-center justify-center px-6 ${textColor}`}
-  style={{ background: gradient }}
->
+      className={`relative w-screen h-screen flex items-center justify-center px-6 ${textColor}`}
+      style={{ background: gradient }}
+    >
+      {/* Navbar */}
+      <div className="absolute top-0 z-30 w-full">
+        <Navbar/>
+      </div>
       {/* Absolute Bulb */}
       <div className="absolute top-0 left-1/2 transform -translate-x-1/2 z-30">
         <Bulb mode={mode} setMode={setMode} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,29 @@
-import logo from "../assets/logo-no-bg.png";
-import logocolor from "../assets/logo-light.png";
+import logo from "/logo-no-bg.png";
+import logocolor from "/logo-light.png";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { SiKofi } from "react-icons/si";
-import { SOCIALLINKS } from "../constants";
+import { SOCIALLINKS } from "../assets/constants";
 
 const Navbar = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6">
-      <div className="flex flex-shrink-0 items-center">
+    <nav className="flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center m-8">
         <img
-          className="w-10 mx-2 dark:hidden"
+          className="w-10 mx-2 logo-light"
           src={logocolor}
-          alt="color logo"
+          alt="Light theme logo"
         />
         <img
-          className="w-10 mx-2 hidden dark:block"
+          className="w-10 mx-2 logo-dark-red"
           src={logo}
-          alt="no bg logo"
+          alt="Dark/Red theme logo"
         />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a
           href="/Aavash Tamang Resume.pdf"
           download
-          className="rounded-sm flex items-center justify-center 
+          className="mb-2 rounded-sm flex items-center justify-center 
                 bg-gradient-to-r from-red-300 via-slate-500 to-purple-500 
                 bg-clip-text text-3xl tracking-tight text-transparent light:from-blue-500 light:via-green-500 light:to-yellow-500 
                 transition-transform transform hover:scale-110"
@@ -34,7 +34,7 @@ const Navbar = () => {
           href={SOCIALLINKS.linkedin}
           target="_blank"
           rel="noopener noreferrer"
-          className="transition-transform transform hover:scale-110 text-gray-900 dark:text-gray-100"
+          className="transition-transform transform hover:scale-110"
         >
           <FaLinkedin />
         </a>
@@ -42,7 +42,7 @@ const Navbar = () => {
           href={SOCIALLINKS.github}
           target="_blank"
           rel="noopener noreferrer"
-          className="transition-transform transform hover:scale-110 text-gray-900 dark:text-gray-100"
+          className="transition-transform transform hover:scale-110"
         >
           <FaGithub />
         </a>
@@ -50,7 +50,7 @@ const Navbar = () => {
           href={SOCIALLINKS.ko_fi}
           target="_blank"
           rel="noopener noreferrer"
-          className="transition-transform transform hover:scale-110 text-gray-900 dark:text-gray-100"
+          className="transition-transform transform hover:scale-110"
         >
           <SiKofi />
         </a>
